fc13fc28274d1fe60e3aab7626aa34fb02b638bb
Supercrafter100 <58982133+supercrafter100@users.noreply.github.com>
2024-03-29 14:41:16 +0100

    add menu buttons for starting, pausing, stopping agent (#21)
    * add menu buttons for starting, pausing, stopping agent
    
    * replace stopAgent with muteAgent
    
    ---------
    
    Co-authored-by: Dipam Sen <funplanet.m4f@gmail.com>

---
+++ src/commands/muteAIAgent.js
@@ -0,0 +1,13 @@
+const Command = require('../lib/command');
+
+class MuteAIAgentCommand extends Command {
+    constructor() {
+        super('bizarro-devin.muteAIAgent');
+    }
+
+    async run() {
+        // Code
+    }
+}
+
+module.exports = MuteAIAgentCommand;
---
+++ src/commands/pauseAIAgent.js
@@ -0,0 +1,13 @@
+const Command = require('../lib/command');
+
+class PauseAIAgentCommand extends Command {
+    constructor() {
+        super('bizarro-devin.pauseAIAgent');
+    }
+
+    async run() {
+        // Code
+    }
+}
+
+module.exports = PauseAIAgentCommand;
