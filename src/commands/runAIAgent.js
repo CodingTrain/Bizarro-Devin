39a7191242ee0d2bdfbb2d7891868f34b269fe9a
Supercrafter100 <58982133+supercrafter100@users.noreply.github.com>
2024-03-30 21:25:36 +0100

    Move agent to its own class

--- src/commands/runAIAgent.js
+++ src/commands/runAIAgent.js
@@ -1,15 +1,8 @@
+const { getAgent } = require('../lib/Agent');
+
 const vscode = require('vscode');
-
 const Command = require('../lib/command');
-const {
-  typeRealistically,
-  typeImmediately,
-} = require('../util/realisticTyping');
-const { speak } = require('../util/speak');
 
-const { query, queryStream } = require('../util/queryModel');
-
-// const { script } = require('../script');
 const { prompts } = require('../prompt');
 
 class RunAIAgentCommand extends Command {
@@ -25,24 +18,10 @@ class RunAIAgentCommand extends Command {
       return;
     }
 
-    const startingPrompt = prompts.startingPrompt;
-    queryStream(startingPrompt, async (response) => {
-      console.log(response);
-      await typeImmediately(editor, response.response);
-    });
-    // const response = await query(startingPrompt);
-    // typeImmediately(editor, response.response);
-    // console.log(response);
-  }
+    const agent = getAgent();
 
-  async processStep(step, editor) {
-    if (step.type === 'code') {
-      // Join the array of strings into a single string separated by newlines, more clear in terms of formatting than the template literal
-      await typeRealistically(editor, step.content);
-      // await typeRealistically(editor, step.content.join('\n'));
-    } else if (step.type === 'narrate') {
-      await speak(step.content);
-    }
+    const startingPrompt = prompts.startingPrompt;
+    agent.prompt(startingPrompt);
   }
 }
 
---
+++ src/lib/Agent.js
@@ -0,0 +1,39 @@
+const { typeImmediately } = require('../util/realisticTyping');
+const vscode = require('vscode');
+const { queryStream } = require('../util/queryModel');
+
+class Agent {
+  constructor() {
+    this.editor = vscode.window.activeTextEditor;
+  }
+
+  /**
+   * Prompt something to the AI
+   * @param {string} input The prompt to be processed
+   */
+  prompt(input) {
+    console.log('prompting', input);
+    queryStream(input, (response) => this.consumeStream(response));
+  }
+
+  async consumeStream(response) {
+    console.log(response);
+    await typeImmediately(this.editor, response.response);
+  }
+}
+
+// Singleton instance of the agent
+let agent = null;
+
+/**
+ * Get the agent instance
+ * @returns {Agent} The agent instance
+ */
+const getAgent = () => {
+  if (!agent) {
+    agent = new Agent();
+  }
+  return agent;
+};
+
+module.exports = { getAgent };
