9370da69a1f12dbe50e41d6c3ae4720cc551b528
Daniel Shiffman <daniel.shiffman@gmail.com>
2024-03-30 23:29:47 -0400

    trying coqui tts-server

---
+++ src/experimental/tts-server.js
@@ -0,0 +1,24 @@
+const player = require('play-sound')();
+const fs = require('fs');
+
+speak('hello');
+
+async function speak(txt) {
+  const response = await fetch(
+    `http://localhost:5002/api/tts?text=${txt}&speaker_id=&style_wav=&language_id=`
+  );
+  const blob = await response.blob();
+  const arrayBuffer = await blob.arrayBuffer();
+  const buffer = Buffer.from(arrayBuffer);
+
+  const tempFilePath = 'temp_audio.wav';
+  fs.writeFileSync(tempFilePath, buffer);
+  player.play(tempFilePath, (err) => {
+    if (err) {
+      console.error('Failed to play:', err);
+    } else {
+      console.log('Audio playback finished.');
+      fs.unlinkSync(tempFilePath);
+    }
+  });
+}
