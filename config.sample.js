7a6521ad2ed36acb8ede7f9b166f03be79aeca90
Supercrafter100 <58982133+supercrafter100@users.noreply.github.com>
2024-04-01 22:04:47 +0200

    add openai as a chat completion provider

--- config.sample.js
+++ config.sample.js
@@ -1,7 +1,8 @@
 // Create a copy of this file and name it config.js
 
 module.exports = {
-  model: 'replicate', // replicate, ollama (currently broken)
+  model: 'replicate', // replicate, ollama, openai
   replicateApiToken: 'your-replicate-api-token-here',
+  openAIApiToken: 'your-openai-api-token-here',
   tts: 'say', // say, coqui
 };
---
+++ src/lib/agent/providers/openaiProvider.js
@@ -0,0 +1,64 @@
+const ModelProvider = require('./genericProvider');
+const { prompts } = require('../../../prompt');
+const config = require('../../../../config');
+const OpenAI = require('openai');
+
+class OllamaProvider extends ModelProvider {
+  constructor() {
+    super();
+    this.openai = new OpenAI({ apiKey: config.openAIApiToken });
+  }
+
+  async query(prompt) {
+    this.messageHistory.push({ role: 'user', content: prompt });
+    const response = await this.openai.chat.completions.create({
+      model: 'gpt-4-turbo-preview',
+      messages: [
+        {
+          role: 'system',
+          content: prompts.systemPrompt,
+        },
+        ...this.messageHistory,
+      ],
+    });
+
+    this.messageHistory.push({
+      role: 'assistant',
+      content: response.choices[0]?.delta?.content,
+    });
+    return {
+      response: response.choices[0]?.delta?.content,
+    };
+  }
+
+  async queryStream(prompt, process) {
+    this.messageHistory.push({ role: 'user', content: prompt });
+    const stream = await this.openai.chat.completions.create({
+      model: 'gpt-4-turbo-preview',
+      messages: [
+        {
+          role: 'system',
+          content: prompts.systemPrompt,
+        },
+        ...this.messageHistory,
+      ],
+      stream: true,
+    });
+
+    let fullResponse = '';
+    for await (const chunk of stream) {
+      if (chunk.choices[0].finish_reason === 'stop') {
+        await process({ response: '', event: 'done' });
+        this.messageHistory.push({ role: 'assistant', content: fullResponse });
+      } else {
+        await process({
+          response: chunk.choices[0]?.delta?.content,
+          event: 'output',
+        });
+        fullResponse += chunk.choices[0]?.delta?.content;
+      }
+    }
+  }
+}
+
+module.exports = OllamaProvider;
