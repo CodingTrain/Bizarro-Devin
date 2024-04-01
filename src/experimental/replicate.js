cb75808b6ac52c9491e9a068d87c954b1438f8bc
Daniel Shiffman <daniel.shiffman@gmail.com>
2024-03-31 20:55:32 -0400

    Testing Llama (#45)
    * testing replicate
    
    * speeding up typing

--- src/experimental/replicate.js
+++ src/experimental/replicate.js
@@ -1,30 +1,41 @@
-const Replicate = require('replicate');
 const config = require('../../config');
-const replicate = new Replicate({
-  auth: config.replicateApiToken,
-});
+const { getProvider } = require('../lib/agent/providers/providerInstance');
 
-async function query(prompt) {
-  const output = await replicate.run('meta/llama-2-7b-chat', {
-    input: { prompt },
-  });
-  return output;
+const provider = getProvider();
+testReplicate();
+
+async function testReplicate() {
+  const response1 = await provider.query('Who are you?');
+  console.log(response1);
+
+  const response2 = await provider.query('Can you code a fractal tree?');
+  console.log(response2);
 }
+// const replicate = new Replicate({
+//   auth: config.replicateApiToken,
+// });
 
-async function queryStream(prompt, process) {
-  const stream = replicate.stream('meta/llama-2-7b-chat', {
-    input: { prompt },
-  });
-  while (true) {
-    const { value } = await stream.next();
-    if (value.event == 'done') break;
-    process({
-      response: value.data, // fake modifying schema to match ollama
-    });
-  }
-}
+// async function query(prompt) {
+//   const output = await replicate.run('meta/llama-2-7b-chat', {
+//     input: { prompt },
+//   });
+//   return output;
+// }
 
-query('Why is the sky blue?').then(console.log).catch(console.error);
+// async function queryStream(prompt, process) {
+//   const stream = replicate.stream('meta/llama-2-7b-chat', {
+//     input: { prompt },
+//   });
+//   while (true) {
+//     const { value } = await stream.next();
+//     if (value.event == 'done') break;
+//     process({
+//       response: value.data, // fake modifying schema to match ollama
+//     });
+//   }
+// }
+
+// query('Why is the sky blue?').then(console.log).catch(console.error);
 
 // queryStream('Why is the sky blue?', (json) => {
 //   console.log(json);
