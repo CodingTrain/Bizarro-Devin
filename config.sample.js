886b675df9de04084c5e63370cd25f85dd4bcae2
Dipam Sen <funplanet.m4f@gmail.com>
2024-04-02 21:26:42 +0530

    Add gemini backend (#54)
    * add tts config
    
    * add config option for tts
    
    * allow AI to update it's existing code
    
    * automatically scroll editor + fix bug with code in done buffer
    
    * add gemini
    
    * add `test-ai` script for testing
    
    * move chat creation to top
    
    * add maxTokens
    
    ---------
    
    Co-authored-by: Supercrafter100 <58982133+supercrafter100@users.noreply.github.com>
    Co-authored-by: Daniel Shiffman <daniel.shiffman@gmail.com>

--- config.sample.js
+++ config.sample.js
@@ -1,8 +1,9 @@
 // Create a copy of this file and name it config.js
 
 module.exports = {
-  model: 'replicate', // replicate, ollama, openai
+  model: 'replicate', // replicate, ollama, openai, gemini
   replicateApiToken: 'your-replicate-api-token-here',
   openAIApiToken: 'your-openai-api-token-here',
+  geminiApiToken: 'your-gemini-api-token-here',
   tts: 'say', // say, coqui
 };
