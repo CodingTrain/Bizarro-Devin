9bd77af89a52284b96a22e211f2e879a7d4d7b4b
Dipam Sen <funplanet.m4f@gmail.com>
2024-04-02 14:20:44 +0530

    move chat creation to top

--- src/lib/agent/providers/geminiProvider.js
+++ src/lib/agent/providers/geminiProvider.js
@@ -8,31 +8,29 @@ class GeminiProvider extends ModelProvider {
     super();
     this.genAI = new GoogleGenerativeAI(config.geminiApiToken);
     this.model = this.genAI.getGenerativeModel({
-      model: 'gemini-pro',
+      model: 'gemini-1.5-pro',
     });
 
     // Gemini manages its own chat history
-    this.chat = null;
+    this.chat = this.model.startChat({
+      history: [
+        {
+          role: 'user', // gemini doesn't have a system role
+          parts: [{ text: prompts.systemPrompt }],
+        },
+        {
+          role: 'model', // fake response
+          parts: [{ text: 'Understood.' }],
+        },
+      ],
+      generationConfig: {
+        // maxOutputTokens:
+      },
+    });
   }
 
   async query(prompt) {
     this.messageHistory.push({ role: 'user', content: prompt });
-
-    if (!this.chat) {
-      this.chat = this.model.startChat({
-        history: [
-          {
-            role: '', // gemini doesn't have a system role
-            parts: [{ text: prompts.systemPrompt }],
-          },
-          {
-            role: 'model', // fake response
-            parts: [{ text: 'Understood.' }],
-          },
-        ],
-        generationConfig: {},
-      });
-    }
 
     const result = await this.chat.sendMessage(prompt);
     const response = await result.response;
@@ -45,21 +43,6 @@ class GeminiProvider extends ModelProvider {
 
   async queryStream(prompt, process) {
     this.messageHistory.push({ role: 'user', content: prompt });
-    if (!this.chat) {
-      this.chat = this.model.startChat({
-        history: [
-          {
-            role: 'user', // gemini doesn't have a system role
-            parts: [{ text: prompts.systemPrompt }],
-          },
-          {
-            role: 'model', // fake response
-            parts: [{ text: 'Understood.' }],
-          },
-        ],
-        generationConfig: {},
-      });
-    }
 
     const result = await this.chat.sendMessageStream(prompt);
     let fullResponse = '';
