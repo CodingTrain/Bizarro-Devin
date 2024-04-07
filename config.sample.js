7ab506d981e09e42189e8f38ff8ee29f22352ffc
Supercrafter100 <58982133+supercrafter100@users.noreply.github.com>
2024-04-07 15:50:27 +0200

    implement playht and elevenlabs (#68)
    * implement playht and elevenlabs
    
    * attempted implementation of elevenlabs websockets. However, can't test due to getting blocked
    
    * forgot some stuff in the example config
    
    * use elevenlabs config variables in streaming implementation
    
    * improve elevenlabs streaming code
    
    * remove ws dependency
    
    * testing saving to file
    
    * add non-streaming method for elevenlabs (elevenlabsSync)
    
    * patch speaker package
    
    ---------
    
    Co-authored-by: Daniel Shiffman <daniel.shiffman@gmail.com>

--- config.sample.js
+++ config.sample.js
@@ -6,5 +6,16 @@ module.exports = {
   replicateApiToken: 'your-replicate-api-token-here',
   openAIApiToken: 'your-openai-api-token-here',
   geminiApiToken: 'your-gemini-api-token-here',
-  tts: 'piper', // say, coqui, piper
+  elevenLabs: {
+    apiKey: 'your-eleven-labs-api-key-here',
+    voiceId: 'your-eleven-labs-voice-id-here',
+    model: 'eleven_turbo_v2',
+    outputFormat: 'pcm_16000',
+    sampleRate: 16000,
+  },
+  playHT: {
+    secret: 'your-playht-secret-here',
+    userId: 'your-playht-user-id-here',
+  },
+  tts: 'piper', // say, coqui, piper, elevenlabs
 };
