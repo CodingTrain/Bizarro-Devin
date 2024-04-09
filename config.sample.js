b120827e056b548aeb5cb9cb5873071607b6909f
Dipam Sen <funplanet.m4f@gmail.com>
2024-04-10 01:54:40 +0530

    fix bugs, add ollamaModel to config (#85)

--- config.sample.js
+++ config.sample.js
@@ -1,10 +1,11 @@
 // Create a copy of this file and name it config.js
 
 module.exports = {
-  model: 'replicate', // replicate, ollama, openai, gemini
+  model: 'openai', // replicate, ollama, openai, gemini
   socketServerPort: 11434, // Port for the socket server
-  tts: 'piper', // say, coqui, piper, elevenlabs, playht
+  tts: 'piper', // say, coqui, piper, elevenlabs, elevenlabsSync, playht
   showSubtitles: false, // show subtitles of what is being said
+  ollamaModel: 'llama2:70b', // model to use with ollama provider (only used if model is ollama)
   replicateApiToken: 'your-replicate-api-token-here',
   openAIApiToken: 'your-openai-api-token-here',
   geminiApiToken: 'your-gemini-api-token-here',
