9a8ee466188b2f6ac3396d9e7cf43a57498ffd49
Daniel Shiffman <daniel.shiffman@gmail.com>
2024-04-13 14:07:51 -0400

    Working on looping sounds (#91)
    * working on looping sound
    
    * looping functionality
    
    * putting back in thinking sound
    
    * add mute button
    
    ---------
    
    Co-authored-by: Supercrafter100 <58982133+supercrafter100@users.noreply.github.com>

--- src/commands/muteAIAgent.js
+++ src/commands/muteAIAgent.js
@@ -1,12 +1,29 @@
 const Command = require('../lib/command');
+const vscode = require('vscode');
+const { toggleMuted, stopSound } = require('../util/sound-effects');
 
 class MuteAIAgentCommand extends Command {
   constructor() {
     super('bizarro-devin.muteAIAgent');
   }
 
+  load(context) {
+    // Include context status item
+    this.contextStatusItem = vscode.window.createStatusBarItem(
+      vscode.StatusBarAlignment.Left,
+      -999
+    );
+    this.contextStatusItem.text = '$(unmute) Sounds enabled';
+    this.contextStatusItem.show();
+    context.subscriptions.push(this.contextStatusItem);
+  }
+
   async run() {
-    // Code
+    const mutedValue = toggleMuted();
+    this.contextStatusItem.text = mutedValue
+      ? '$(mute) Sounds disabled'
+      : '$(unmute) Sounds enabled';
+    stopSound();
   }
 }
 
--- src/util/sound-effects.js
+++ src/util/sound-effects.js
@@ -16,24 +16,51 @@ let sounds = {
   ),
 };
 
-function playSound(sound) {
+let currentSound = null;
+let looping = false;
+let muted = false;
+
+function playSound(sound, loop = false) {
+  if (muted) return Promise.resolve();
+
   const filepath = sounds[sound];
+  looping = loop;
   return new Promise((resolve, reject) => {
-    player.play(filepath, (err) => {
-      if (err) {
-        console.error('Failed to play:', err);
-        reject(err);
-      } else {
-        // console.log('Audio playback finished.');
-        // Hanging on this, not sure why
-        // fs.unlinkSync(tempFilePath);
-        resolve();
-      }
-    });
+    function startSound() {
+      currentSound = player.play(filepath, (err) => {
+        if (err) {
+          console.error('Failed to play:', err);
+          reject(err);
+        } else {
+          if (looping && !muted) {
+            startSound();
+          } else {
+            // Hanging on this, not sure why
+            // fs.unlinkSync(tempFilePath);
+            resolve();
+          }
+        }
+      });
+    }
+    startSound();
   });
+}
+
+function stopSound() {
+  if (currentSound && looping) {
+    currentSound.kill();
+    looping = false;
+  }
+}
+
+function toggleMuted() {
+  muted = !muted;
+  return muted;
 }
 
 module.exports = {
   // sounds,
   playSound,
+  stopSound,
+  toggleMuted,
 };
