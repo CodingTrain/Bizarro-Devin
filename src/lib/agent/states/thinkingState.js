bc3ac2e71d11c537b6013e513e74fc5194541d3d
Daniel Shiffman <daniel.shiffman@gmail.com>
2024-04-12 21:23:29 -0400

    putting back in thinking sound

--- src/lib/agent/states/thinkingState.js
+++ src/lib/agent/states/thinkingState.js
@@ -1,12 +1,16 @@
 const { setStatusbarText } = require('../../../extension');
 const State = require('../../../util/statemachine/State');
-// const { playSound } = require('../../../util/sound-effects');
+const { playSound, stopSound } = require('../../../util/sound-effects');
 
 class ThinkingState extends State {
   onActivate() {
     setStatusbarText('$(loading~spin) waiting for chunks...');
     this.stateMachine.webserver.sendStatus('thinking'); // state machine is our agent in this case
-    // playSound('thinking');
+    playSound('thinking', true);
+  }
+
+  onDeactivate() {
+    stopSound();
   }
 }
 
